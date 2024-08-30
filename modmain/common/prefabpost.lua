f0092f36bc7bbef9c12561f96fb895752c4f16c3
Lunarsolar <lunarsolar6174@126.com>
2024-08-30 11:18:00 +0800

    碎裂装备改动

--- modmain/common/prefabpost.lua
+++ modmain/common/prefabpost.lua
@@ -727,3 +727,33 @@ end)
 
 
 ----------------------------------------------------------------------------------------------------
+
+local function shardDMGRedirect(self, attacker, damage, weapon, ...) -- 碎裂武器伤害重定向
+    if weapon then
+        if weapon.prefab == "shard_sword" and self.inst:HasTag("shadow") then -- 碎裂剑对梦魇生物
+            local health = self.inst.components.health
+            if health then
+                if health.currenthealth <= TUNING.SWP_SHARD_DMG.SWORD_MAX then
+                    return nil, false, {self, attacker,
+                                        math.max(TUNING.SWP_SHARD_DMG.SWORD2SHADOW, health.currenthealth - 1), weapon,
+                                        ...}
+                else
+                    if attacker and attacker.components.combat then
+                        attacker:DoTaskInTime(0, function()
+                            attacker.components.combat:GetAttacked(weapon, math.random(1, 5))
+                            attacker:PushEvent("thorns")
+                        end)
+                    end
+                    return nil, false, {self, attacker, TUNING.SWP_SHARD_DMG.SWORD_MAX, weapon, ...}
+                end
+            end
+        elseif weapon.prefab == "shard_beak" and -- 碎裂喙对建筑和巢
+            (self.inst:HasTag("wall") or self.inst:HasTag("structure") or self.inst.components.childspawner) then
+            return nil, false, {self, attacker, TUNING.SWP_SHARD_DMG.BEAK2STRUTURE, weapon, ...}
+        end
+    end
+end
+
+AddComponentPostInit("combat", function(self, inst) Utils.FnDecorator(self, "GetAttacked", shardDMGRedirect) end)
+
+----------------------------------------------------------------------------------------------------
--- scripts/prefabs/glass.lua
+++ scripts/prefabs/glass.lua
@@ -1,37 +1,9 @@
-local assets =
-{
-    Asset("ANIM", "anim/des_shard_sword_ground.zip"),
-    Asset("ANIM", "anim/swap_des_shard_sword.zip"),
-    Asset("ANIM", "anim/des_shard_beak_ground.zip"),
-    Asset("ANIM", "anim/swap_des_shard_beak.zip"),
-    Asset("ANIM", "anim/des_glass_shards.zip"),
-}
+local assets = {Asset("ANIM", "anim/des_shard_sword_ground.zip"), Asset("ANIM", "anim/swap_des_shard_sword.zip"),
+                Asset("ANIM", "anim/des_shard_beak_ground.zip"), Asset("ANIM", "anim/swap_des_shard_beak.zip"),
+                Asset("ANIM", "anim/des_glass_shards.zip")}
 
-local day_time = 60 * 8
-local total_day_time = day_time
-local wilson_attack = 34
-local wilson_health = 150
-local calories_per_day = 75
-
-
-
----spear damage = 34
----ham bat damage = 60
----nightsword damage = 68
-
-------------GLASS BEAK--------------
-DES_GLASS_BEAK_DAMAGE = 34
-DES_GLASS_BEAK_DAMAGE_VS_STRUCTURE = DES_GLASS_BEAK_DAMAGE * 5
-DES_GLASS_BEAK_DURABILITY = 100 --- it has crow in it hard to craft -- Runar: DST哪个版本时做的碎裂喙？难做是什么意思
-
-------------GLASS SWORD--------------
-DES_GLASS_SWORD_THRONS = math.random(1, 5)
-DES_GLASS_SWORD_DAMAGE = 50
-DES_GLASS_SWORD_DAMAGE_MAX = 400
-DES_GLASS_SWORD_DAMAGE_VS_SHADOW = 100 -- Runar: 原本的斩杀线
-DES_GLASS_SWORD_DURABILITY = 50
-
-
+local BEAK_DURABILITY = 100
+local SWORD_DURABILITY = BEAK_DURABILITY / 2
 
 local function shine(inst)
     inst.task = nil
@@ -40,58 +12,25 @@ local function shine(inst)
     inst.task = inst:DoTaskInTime(4 + math.random() * 5, shine)
 end
 
-local function onequip1(inst, owner)
+local function swordequip(inst, owner)
     owner.AnimState:OverrideSymbol("swap_object", "swap_des_shard_sword", "swap_des_shard_sword")
     owner.AnimState:Show("ARM_carry")
     owner.AnimState:Hide("ARM_normal")
 end
 
-local function onunequip1(inst, owner)
-    owner.AnimState:Hide("ARM_carry")
-    owner.AnimState:Show("ARM_normal")
-end
-
-
-local function onattack1(inst, owner, target)
-    if target.components.combat and target.components.health then
-        if target:HasTag("shadow") then
-            if target.components.health.currenthealth <= DES_GLASS_SWORD_DAMAGE_MAX then
-                target.components.combat:GetAttacked(owner,
-                    math.max(DES_GLASS_SWORD_DAMAGE_VS_SHADOW, target.components.health.currenthealth - 1), inst)
-            else
-                target.components.combat:GetAttacked(owner, DES_GLASS_SWORD_DAMAGE_MAX, inst)
-                if owner and owner.components.combat and owner.components.health and not owner.components.health:IsDead() then
-                    owner.components.combat:GetAttacked(owner, DES_GLASS_SWORD_THRONS, inst)
-                end
-            end
-        else
-            target.components.combat:GetAttacked(owner, DES_GLASS_SWORD_DAMAGE, inst)
-        end
-    end
-end
-
-local function onequip(inst, owner)
+local function beakequip(inst, owner)
     owner.AnimState:OverrideSymbol("swap_object", "swap_des_shard_beak", "swap_des_shard_beak")
     owner.AnimState:Show("ARM_carry")
     owner.AnimState:Hide("ARM_normal")
 end
 
 local function onunequip(inst, owner)
+    owner.AnimState:ClearOverrideSymbol("swap_object")
     owner.AnimState:Hide("ARM_carry")
     owner.AnimState:Show("ARM_normal")
 end
 
-local function onattack(inst, owner, target)
-    if target.components.health then
-        if target:HasTag("wall") or target:HasTag("structure") or target.components.childspawner then --Runar 打墙打巢都很快
-            target.components.combat:GetAttacked(owner, DES_GLASS_BEAK_DAMAGE_VS_STRUCTURE, inst)
-        else
-            target.components.combat:GetAttacked(owner, DES_GLASS_BEAK_DAMAGE, inst)
-        end
-    end
-end
-
-local function fnshard_sword()
+local function common()
     local inst = CreateEntity()
 
     inst.entity:AddTransform()
@@ -99,6 +38,21 @@ local function fnshard_sword()
     inst.entity:AddNetwork()
 
     MakeInventoryPhysics(inst)
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then return inst end
+
+    inst:AddComponent("inspectable")
+
+    inst:AddComponent("inventoryitem")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/volcanoinventory.xml"
+
+    return inst
+end
+
+local function swordfn()
+    local inst = common()
 
     inst.AnimState:SetBank("des_shard_sword_ground")
     inst.AnimState:SetBuild("des_shard_sword_ground")
@@ -107,47 +61,27 @@ local function fnshard_sword()
     inst:AddTag("sharp")
     MakeInventoryFloatable(inst)
 
-    inst.entity:SetPristine()
-
-    if not TheWorld.ismastersim then
-        return inst
-    end
+    if not TheWorld.ismastersim then return inst end
 
     inst:AddComponent("weapon")
-    inst.components.weapon:SetDamage(0) -- Runar: 不知道怎么让它既显示攻击力又不造成伤害
-    inst.components.weapon.onattack = onattack1
-
-    -------
+    inst.components.weapon:SetDamage(TUNING.SWP_SHARD_DMG.SWORD) 
 
     inst:AddComponent("finiteuses")
-    inst.components.finiteuses:SetMaxUses(DES_GLASS_SWORD_DURABILITY)
-    inst.components.finiteuses:SetUses(DES_GLASS_SWORD_DURABILITY)
+    inst.components.finiteuses:SetMaxUses(SWORD_DURABILITY)
+    inst.components.finiteuses:SetUses(SWORD_DURABILITY)
     inst.components.finiteuses:SetOnFinished(inst.Remove)
 
-    inst:AddComponent("inspectable")
-
-    inst:AddComponent("inventoryitem")
-    inst.components.inventoryitem.atlasname = "images/inventoryimages/volcanoinventory.xml"
-
-
     inst:AddComponent("equippable")
-    inst.components.equippable:SetOnEquip(onequip1)
-    inst.components.equippable:SetOnUnequip(onunequip1)
+    inst.components.equippable:SetOnEquip(swordequip)
+    inst.components.equippable:SetOnUnequip(onunequip)
 
     MakeHauntableLaunch(inst)
 
     return inst
 end
 
-
-local function fnshard_beak()
-    local inst = CreateEntity()
-
-    inst.entity:AddTransform()
-    inst.entity:AddAnimState()
-    inst.entity:AddNetwork()
-
-    MakeInventoryPhysics(inst)
+local function beakfn()
+    local inst = common()
 
     inst.AnimState:SetBank("des_shard_beak_ground")
     inst.AnimState:SetBuild("des_shard_beak_ground")
@@ -155,32 +89,18 @@ local function fnshard_beak()
 
     inst:AddTag("sharp")
 
-    inst.entity:SetPristine()
-
-    if not TheWorld.ismastersim then
-        return inst
-    end
+    if not TheWorld.ismastersim then return inst end
 
     inst:AddComponent("weapon")
-    inst.components.weapon:SetDamage(0) -- Runar: 放弃多段伤害
-    inst.components.weapon.onattack = onattack
-
-    -------
+    inst.components.weapon:SetDamage(TUNING.SWP_SHARD_DMG.BEAK) 
 
     inst:AddComponent("finiteuses")
-    inst.components.finiteuses:SetMaxUses(DES_GLASS_BEAK_DURABILITY)
-    inst.components.finiteuses:SetUses(DES_GLASS_BEAK_DURABILITY)
+    inst.components.finiteuses:SetMaxUses(BEAK_DURABILITY)
+    inst.components.finiteuses:SetUses(BEAK_DURABILITY)
     inst.components.finiteuses:SetOnFinished(inst.Remove)
 
-    inst:AddComponent("inspectable")
-
-    inst:AddComponent("inventoryitem")
-    inst.components.inventoryitem.atlasname = "images/inventoryimages/volcanoinventory.xml"
-
-
-
     inst:AddComponent("equippable")
-    inst.components.equippable:SetOnEquip(onequip)
+    inst.components.equippable:SetOnEquip(beakequip)
     inst.components.equippable:SetOnUnequip(onunequip)
 
     MakeHauntableLaunch(inst)
@@ -189,14 +109,9 @@ local function fnshard_beak()
 end
 
 local function fn()
-    local inst = CreateEntity()
+    local inst = common()
 
-    inst.entity:AddTransform()
-    inst.entity:AddAnimState()
     inst.entity:AddSoundEmitter()
-    inst.entity:AddNetwork()
-
-    MakeInventoryPhysics(inst)
 
     inst.AnimState:SetRayTestOnBB(true)
     inst.AnimState:SetBank("des_glass_shards")
@@ -207,14 +122,7 @@ local function fn()
     inst:AddTag("renewable")
     inst:AddTag("quakedebris")
 
-
-
-
-    inst.entity:SetPristine()
-
-    if not TheWorld.ismastersim then
-        return inst
-    end
+    if not TheWorld.ismastersim then return inst end
 
     inst:AddComponent("edible")
     inst.components.edible.foodtype = FOODTYPE.ELEMENTAL
@@ -224,21 +132,14 @@ local function fn()
     inst:AddComponent("stackable")
     inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
 
-    inst:AddComponent("inspectable")
-
-    inst:AddComponent("inventoryitem")
-    inst.components.inventoryitem.atlasname = "images/inventoryimages/volcanoinventory.xml"
-
-
     MakeHauntableLaunchAndSmash(inst)
 
     inst:AddComponent("bait")
-    print("COLOR CHANGE")
-    local s = math.random() * 0.3 + 0.9
-    local r = math.random() * 0.4 + 0.6
-    local g = math.random() * 0.4 + 0.5
-    local b = math.random() * 0.4 + 0.6
-    local a = math.random() * 0.3 + 0.7
+    local s = math.random() * .3 + .9
+    local r = math.random() * .4 + .6
+    local g = math.random() * .4 + .5
+    local b = math.random() * .4 + .6
+    local a = math.random() * .3 + .7
 
     inst.AnimState:SetScale(s, s, s, s)
     inst.AnimState:SetMultColour(r, g, b, 1)
@@ -248,6 +149,4 @@ local function fn()
     return inst
 end
 
-return Prefab("shard_sword", fnshard_sword, assets),
-    Prefab("shard_beak", fnshard_beak, assets),
-    Prefab("glass_shards", fn, assets)
+return Prefab("shard_sword", swordfn, assets), Prefab("shard_beak", beakfn, assets), Prefab("glass_shards", fn, assets)
