2fea2f9770b0985c5bc3cc5082897787d73fe2cb
momo <niuzw18@foxmail.com>
2025-02-12 10:06:51 +0800

    劈砍兼容排队论

--- modmain/common/prefabpost.lua
+++ modmain/common/prefabpost.lua
@@ -1209,3 +1209,13 @@ ACTIONS.TERRAFORM.fn = function(act)
 	end
 	return old_terraform(act)
 end
+
+-- for client mod ActionQueue
+AddComponentPostInit("actionqueuer", function(self)
+    if self.AddActionList then
+        self.AddActionList("allclick", "HACK", "HACK1")
+        self.AddActionList("leftclick", "HACK", "HACK1")
+        self.AddActionList("autocollect", "HACK", "HACK1")
+        self.AddActionList("noworkdelay", "HACK", "HACK1")
+    end
+end)
