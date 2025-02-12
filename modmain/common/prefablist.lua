23f1e523d36d7f142a5fc31d43affce74d06d19e
LunarSolar <kageno13927@yeah.net>
2025-02-12 14:39:03 +0800

    对解毒剂进行修改,使其能用于给牛解毒

--- modmain/common/prefablist.lua
+++ modmain/common/prefablist.lua
@@ -1,116 +1,107 @@
-PrefabFiles =
-{
-    "wall_invisible",
-    "interior_center",
-    "shipwrecked_boat",
-    "shipwrecked_boat_placer",
-    "shipwrecked_boat_placer2",
-    "sail",
-    "tropical_boatlamp",
-
-    --"turfesvolcanobiome",
-    "venomgland",
-    "machetes",
-    "antivenom",
-    "splash_water",
-    "debris", --debris_1, debris_2, debris_3, debris_4 espalhar na praia
-
-    "firetwister",
-    "twister_tornadodefogo",
-    "firetwister_seal",
-    "glass",
-    "flood_ice",
-    "mushroom_yellow",		
-    "mushtree_yellow",
-    "mushtree_spores_yellow",
-    "bishopwater",
-    "seacucumber",
-    "watercress",
-    "pinkman",
-    "piggolem",
-    "jellyfish",
-    "feathers_tro",
-    "boatmetal",
-    --"bird_swarm",
-    "tropicalspawnblocker",
-    -- "swfishbait", --热带鱼饵
+PrefabFiles = {
+	"fryfocals_charge",
+	"goggles",
+	"hiddendanger_fx",
+	"thumper",
+	"tro_veggies",
+    "antitoxins",
+    "armor_void_cloak",
+    "artic_flower",
+    "beaverskin",
+    "beavertorch",
     "beds",
-    "icebearger",
-    "icedeerclops",
-    "icerockcreatures",
-    "cave_exit_vulcao",
-    "cave_entrance_vulcao",
-    "vidanomar",
-    "vidanomarseaworld",
+    "bishopwater",
     "boat_raft_rot",
-    "piggravestone",
-    "mangrovespawner",
-    "oxwaterspawner",
-    "grasswaterspawner",
-    "waterreedspawner",
-    "fishinholewaterspawner",
+    "boatmetal",
+    "bramble_bush",
+    "buffs_tro", -- (Food) Buffs
     "bundled_structure",
-    "spider_mutators_new",
+    "cave_entrance_vulcao",
+    "cave_exit_vulcao",
+    "crabapple_tree",
+    "debris",
+    "feathers_tro",
+    "fennel",
+    "firetwister_seal",
+    "firetwister",
+    "fishinholewaterspawner",
+    "flood_ice",
+    "glass",
+    "gorge_portal",
+    "grasswaterspawner",
     "grotto_grub_nest",
     "grotto_grub",
     "grotto_parsnip",
     "grottoqueen",
+    "icebearger",
+    "icedeerclops",
     "icedpad",
-    --"yeti",
-    "artic_flower",
-    "watertree_pillar2",
-    "bramble_bush",
-    "tidalpoolnew",
-    "marsh_tree_new",
+    "icerockcreatures",
+    "interior_center",
+    "jellyfish",
     "ligamundo",
-    "armor_void_cloak",
-    "gorge_portal",
-    "volcano_altar_pillar",
-    "pollen_item",
+    "machetes",
+    "mangrovespawner",
+    "marsh_tree_new",
+    "maxwellboss",
+    "maxwellendgame",
+    "maxwellestatua",
+    "maxwelllight_flame",
+    "maxwelllight",
+    "maxwelllock",
+    "maxwellminions",
+    "maxwellphonograph",
+    "maxwellshadowheart",
+    "maxwellshadowmeteor",
     "mermhouse_tropical",
     "mermtrader",
-    "wildbeaver",
-    "wildbeaver_house",
-    "wildbeaverguard",
-    "beaverskin",
-    "stick_heads",
-    "beavertorch",
+    "mushroom_yellow",		
+    "mushtree_spores_yellow",
+    "mushtree_yellow",
     "obsidiancoconade",
     "obsidiancoconadeactive",
-    "maxwellendgame",
-    "maxwelllight",
-    "maxwelllight_flame",
-    "maxwellminions",
-    "maxwellboss",
-    "maxwelllock",
-    "maxwellshadowmeteor",
-    "maxwellphonograph",
-    "maxwellestatua",
-    "maxwellshadowheart",
-    "tree_forest",
-    "tree_forest_deep",
-    "tree_forest_rot",
-    "tree_forestseed",
-    "trapslug",
-    "fennel",
+    "oxwaterspawner",
     "pig_palace2",
-    "slipstor",
+    "pig_shop_spears",
+    "piggolem",
+    "piggravestone",
+    "pigkingstaff",
+    "pinkman",
+    "pollen_item",
+    "sail",
+    "seacucumber",
+    "shipwrecked_boat_placer",
+    "shipwrecked_boat_placer2",
+    "shipwrecked_boat",
     "slip",
     "slipstor_spawner",
-    "pig_shop_spears",
-    "crabapple_tree",
-    "wildboreking",
-    "wildbore_minion",
-    "pigkingstaff",
-    "wildboreking_spawner",
+    "slipstor",
+    "spider_mutators_new",
+    "splash_water",
+    "stick_heads",
+    "tidalpoolnew",
+    "trapslug",
+    "tree_forest_deep",
+    "tree_forest_rot",
+    "tree_forest",
+    "tree_forestseed",
     "tro_treasurechest",
-    "buffs_tro", -- (Food) Buffs
-	"tro_veggies",
-	"fryfocals_charge",
-	"hiddendanger_fx",
-	"goggles",
-	"thumper",
-
+    "tropical_boatlamp",
+    "tropicalspawnblocker",
+    "twister_tornadodefogo",
+    "vidanomar",
+    "vidanomarseaworld",
+    "volcano_altar_pillar",
+    "wall_invisible",
+    "watercress",
+    "waterreedspawner",
+    "watertree_pillar2",
+    "wildbeaver_house",
+    "wildbeaver",
+    "wildbeaverguard",
+    "wildbore_minion",
+    "wildboreking_spawner",
+    "wildboreking",
 }
 
 if GetModConfigData("whirlpools") then
@@ -123,152 +114,144 @@ end
 
 ---------------lillypad biome------------------------
 if GetModConfigData("lilypad") then
+    table.insert(PrefabFiles, "bill_quill")
+    table.insert(PrefabFiles, "bill")
     table.insert(PrefabFiles, "hippo_antler")
     table.insert(PrefabFiles, "hippoherd")
     table.insert(PrefabFiles, "hippoptamoose")
     table.insert(PrefabFiles, "lillypad")
-    table.insert(PrefabFiles, "lotus")
     table.insert(PrefabFiles, "lotus_flower")
-    table.insert(PrefabFiles, "bill")
-    table.insert(PrefabFiles, "bill_quill")
+    table.insert(PrefabFiles, "lotus")
     table.insert(PrefabFiles, "reeds_water")
 end
+
 ---------------lavaarena volcano---------------------
-
 if TUNING.tropical.hamlet_caves then
-    table.insert(PrefabFiles, "topiary")
-    table.insert(PrefabFiles, "lawnornaments")
-    table.insert(PrefabFiles, "hedge")
-    table.insert(PrefabFiles, "clippings")
-    table.insert(PrefabFiles, "city_lamp")
-    table.insert(PrefabFiles, "hamlet_cones")
-    table.insert(PrefabFiles, "securitycontract")
-    table.insert(PrefabFiles, "city_hammer")
-    table.insert(PrefabFiles, "magnifying_glass")
-    table.insert(PrefabFiles, "pigbandit")
-    table.insert(PrefabFiles, "banditmap")
-    table.insert(PrefabFiles, "pig_shop")
-
-    table.insert(PrefabFiles, "pigman_shopkeeper_desk")
-    table.insert(PrefabFiles, "shop_pedestals")
-    table.insert(PrefabFiles, "deed")
-    table.insert(PrefabFiles, "shelf")
-    table.insert(PrefabFiles, "shelf_slot")
-    table.insert(PrefabFiles, "trinkets_giftshop")
-    table.insert(PrefabFiles, "key_to_city")
-    table.insert(PrefabFiles, "player_house_kits")
-    table.insert(PrefabFiles, "player_house")
-
-    table.insert(PrefabFiles, "pigman_city")
-    table.insert(PrefabFiles, "pighouse_city")
-    table.insert(PrefabFiles, "pig_guard_tower")
-    table.insert(PrefabFiles, "armor_metal")
-    table.insert(PrefabFiles, "reconstruction_project")
-    table.insert(PrefabFiles, "water_spray")
-    table.insert(PrefabFiles, "water_pipe")
-    table.insert(PrefabFiles, "sprinkler1")
     table.insert(PrefabFiles, "alloy")
-    table.insert(PrefabFiles, "smelter")
+    table.insert(PrefabFiles, "armor_metal")
+    table.insert(PrefabFiles, "banditmap")
+    table.insert(PrefabFiles, "city_hammer")
+    table.insert(PrefabFiles, "city_lamp")
+    table.insert(PrefabFiles, "clippings")
+    table.insert(PrefabFiles, "deed")
     table.insert(PrefabFiles, "halberd")
+    table.insert(PrefabFiles, "hamlet_cones")
+    table.insert(PrefabFiles, "hedge")
+    table.insert(PrefabFiles, "key_to_city")
+    table.insert(PrefabFiles, "lawnornaments")
+    table.insert(PrefabFiles, "magnifying_glass")
     table.insert(PrefabFiles, "oinc")
     table.insert(PrefabFiles, "oinc10")
     table.insert(PrefabFiles, "oinc100")
+    table.insert(PrefabFiles, "pig_guard_tower")
+    table.insert(PrefabFiles, "pig_shop")
+    table.insert(PrefabFiles, "pigbandit")
+    table.insert(PrefabFiles, "pighouse_city")
+    table.insert(PrefabFiles, "pigman_city")
+    table.insert(PrefabFiles, "pigman_shopkeeper_desk")
+    table.insert(PrefabFiles, "player_house_kits")
+    table.insert(PrefabFiles, "player_house")
+    table.insert(PrefabFiles, "reconstruction_project")
+    table.insert(PrefabFiles, "securitycontract")
+    table.insert(PrefabFiles, "shelf_slot")
+    table.insert(PrefabFiles, "shelf")
+    table.insert(PrefabFiles, "shop_pedestals")
+    table.insert(PrefabFiles, "smelter")
+    table.insert(PrefabFiles, "sprinkler1")
+    table.insert(PrefabFiles, "topiary")
+    table.insert(PrefabFiles, "trinkets_giftshop")
+    table.insert(PrefabFiles, "water_pipe")
+    table.insert(PrefabFiles, "water_spray")
 end
 
 if TUNING.tropical.multiplayerportal == 15 then
     table.insert(PrefabFiles, "porklandintro")
 end
 
-
 if TUNING.tropical.multiplayerportal == 15
     or TUNING.tropical.hamlet_caves
 then
-    table.insert(PrefabFiles, "rainforesttrees")
-    table.insert(PrefabFiles, "rainforesttree_sapling")
-    table.insert(PrefabFiles, "meteor_impact")
-    table.insert(PrefabFiles, "tuber")
-    table.insert(PrefabFiles, "tubertrees")
-    table.insert(PrefabFiles, "pangolden")
-    table.insert(PrefabFiles, "thunderbird")
-    table.insert(PrefabFiles, "thunderbirdnest")
-    table.insert(PrefabFiles, "ancient_robots")
-    table.insert(PrefabFiles, "ancient_hulk")
+    table.insert(PrefabFiles, "adult_flytrap")
+    table.insert(PrefabFiles, "aloe")
     table.insert(PrefabFiles, "ancient_herald")
-    table.insert(PrefabFiles, "armor_vortex_cloak")
-    table.insert(PrefabFiles, "living_artifact")
-    table.insert(PrefabFiles, "rock_basalt")
+    table.insert(PrefabFiles, "ancient_hulk")
     table.insert(PrefabFiles, "ancient_robots_assembly")
-    table.insert(PrefabFiles, "laser_ring")
-    table.insert(PrefabFiles, "laser")
-    table.insert(PrefabFiles, "iron")
-    table.insert(PrefabFiles, "pheromonestone")
-
+    table.insert(PrefabFiles, "ancient_robots")
+    table.insert(PrefabFiles, "antchest")
+    table.insert(PrefabFiles, "antcombhomecave")
     table.insert(PrefabFiles, "anthill_lamp")
     table.insert(PrefabFiles, "anthill_stalactite")
-    table.insert(PrefabFiles, "antchest")
-    table.insert(PrefabFiles, "collapsed_honeychest")
-    table.insert(PrefabFiles, "corkchest")
-    table.insert(PrefabFiles, "antcombhomecave")
-    table.insert(PrefabFiles, "giantgrub")
-    table.insert(PrefabFiles, "antqueen")
-    table.insert(PrefabFiles, "rocksham")
-    table.insert(PrefabFiles, "deco_ruins_fountain")
-
-    table.insert(PrefabFiles, "pig_ruins_dart_statue")
-    table.insert(PrefabFiles, "pig_ruins_dart")
-    table.insert(PrefabFiles, "pig_ruins_pressure_plate")
-    table.insert(PrefabFiles, "pig_ruins_spear_trap")
-    table.insert(PrefabFiles, "smashingpot")
-    table.insert(PrefabFiles, "pig_ruins_light_beam")
-    table.insert(PrefabFiles, "littlehammer")
-    table.insert(PrefabFiles, "relics")
-    table.insert(PrefabFiles, "gascloud")
-    table.insert(PrefabFiles, "bugrepellent")
-
-
-    table.insert(PrefabFiles, "teatree_nut")
-    table.insert(PrefabFiles, "teatrees")
-    table.insert(PrefabFiles, "piko")
-    table.insert(PrefabFiles, "clawpalmtrees")
-    table.insert(PrefabFiles, "clawpalmtree_sapling")
-    table.insert(PrefabFiles, "bugfood")
-    table.insert(PrefabFiles, "rock_flippable")
-    table.insert(PrefabFiles, "peagawkfeather")
-    table.insert(PrefabFiles, "peagawk")
-    table.insert(PrefabFiles, "aloe")
-    table.insert(PrefabFiles, "pog")
-    table.insert(PrefabFiles, "weevole")
-    table.insert(PrefabFiles, "weevole_carapace")
-    table.insert(PrefabFiles, "armor_weevole")
-    table.insert(PrefabFiles, "pugalisk")
-    table.insert(PrefabFiles, "pugalisk_trap_door")
-    table.insert(PrefabFiles, "pugalisk_ruins_pillar")
-    table.insert(PrefabFiles, "pugalisk_fountain")
-    table.insert(PrefabFiles, "waterdrop")
-    table.insert(PrefabFiles, "floweroflife")
-    table.insert(PrefabFiles, "gaze_beam")
-    table.insert(PrefabFiles, "snake_bone")
-    table.insert(PrefabFiles, "mandrakehouse")
-    table.insert(PrefabFiles, "mandrakeman")
-
-    table.insert(PrefabFiles, "jungle_border_vine")
-    table.insert(PrefabFiles, "light_rays_ham")
-    table.insert(PrefabFiles, "nettle")
-    table.insert(PrefabFiles, "nettle_plant")
-    table.insert(PrefabFiles, "rainforesttrees")
-    table.insert(PrefabFiles, "rainforesttree_sapling")
-    table.insert(PrefabFiles, "tree_pillar")
-    table.insert(PrefabFiles, "flower_rainforest")
-    table.insert(PrefabFiles, "pig_ruins_torch")
-    table.insert(PrefabFiles, "mean_flytrap")
-    table.insert(PrefabFiles, "radish")
-    table.insert(PrefabFiles, "adult_flytrap")
+    table.insert(PrefabFiles, "antlarva")
     table.insert(PrefabFiles, "antman_warrior_egg")
     table.insert(PrefabFiles, "antman_warrior")
     table.insert(PrefabFiles, "antman")
-    table.insert(PrefabFiles, "antlarva")
+    table.insert(PrefabFiles, "antqueen")
     table.insert(PrefabFiles, "antsuit")
+    table.insert(PrefabFiles, "armor_vortex_cloak")
+    table.insert(PrefabFiles, "armor_weevole")
+    table.insert(PrefabFiles, "bugfood")
+    table.insert(PrefabFiles, "bugrepellent")
+    table.insert(PrefabFiles, "clawpalmtree_sapling")
+    table.insert(PrefabFiles, "clawpalmtrees")
+    table.insert(PrefabFiles, "cloudpuff")
+    table.insert(PrefabFiles, "collapsed_honeychest")
+    table.insert(PrefabFiles, "corkchest")
+    table.insert(PrefabFiles, "deco_ruins_fountain")
+    table.insert(PrefabFiles, "flower_rainforest")
+    table.insert(PrefabFiles, "floweroflife")
+    table.insert(PrefabFiles, "gascloud")
+    table.insert(PrefabFiles, "gaze_beam")
+    table.insert(PrefabFiles, "giantgrub")
+    table.insert(PrefabFiles, "iron")
+    table.insert(PrefabFiles, "jungle_border_vine")
+    table.insert(PrefabFiles, "laser_ring")
+    table.insert(PrefabFiles, "laser")
+    table.insert(PrefabFiles, "light_rays_ham")
+    table.insert(PrefabFiles, "littlehammer")
+    table.insert(PrefabFiles, "living_artifact")
+    table.insert(PrefabFiles, "mandrakehouse")
+    table.insert(PrefabFiles, "mandrakeman")
+    table.insert(PrefabFiles, "mean_flytrap")
+    table.insert(PrefabFiles, "meteor_impact")
+    table.insert(PrefabFiles, "nettle_plant")
+    table.insert(PrefabFiles, "nettle")
+    table.insert(PrefabFiles, "pangolden")
+    table.insert(PrefabFiles, "peagawk")
+    table.insert(PrefabFiles, "peagawkfeather")
+    table.insert(PrefabFiles, "pheromonestone")
+    table.insert(PrefabFiles, "pig_ruins_dart_statue")
+    table.insert(PrefabFiles, "pig_ruins_dart")
+    table.insert(PrefabFiles, "pig_ruins_light_beam")
+    table.insert(PrefabFiles, "pig_ruins_pressure_plate")
+    table.insert(PrefabFiles, "pig_ruins_spear_trap")
+    table.insert(PrefabFiles, "pig_ruins_torch")
+    table.insert(PrefabFiles, "piko")
+    table.insert(PrefabFiles, "pog")
+    table.insert(PrefabFiles, "pugalisk_fountain")
+    table.insert(PrefabFiles, "pugalisk_ruins_pillar")
+    table.insert(PrefabFiles, "pugalisk_trap_door")
+    table.insert(PrefabFiles, "pugalisk")
+    table.insert(PrefabFiles, "radish")
+    table.insert(PrefabFiles, "rainforesttree_sapling")
+    table.insert(PrefabFiles, "rainforesttree_sapling")
+    table.insert(PrefabFiles, "rainforesttrees")
+    table.insert(PrefabFiles, "rainforesttrees")
+    table.insert(PrefabFiles, "relics")
+    table.insert(PrefabFiles, "rock_basalt")
+    table.insert(PrefabFiles, "rock_flippable")
+    table.insert(PrefabFiles, "rocksham")
+    table.insert(PrefabFiles, "smashingpot")
+    table.insert(PrefabFiles, "snake_bone")
+    table.insert(PrefabFiles, "teatree_nut")
+    table.insert(PrefabFiles, "teatrees")
+    table.insert(PrefabFiles, "thunderbird")
+    table.insert(PrefabFiles, "thunderbirdnest")
+    table.insert(PrefabFiles, "tree_pillar")
+    table.insert(PrefabFiles, "tuber")
+    table.insert(PrefabFiles, "tubertrees")
     table.insert(PrefabFiles, "venus_stalk")
     table.insert(PrefabFiles, "walkingstick")
-    table.insert(PrefabFiles, "cloudpuff")
+    table.insert(PrefabFiles, "waterdrop")
+    table.insert(PrefabFiles, "weevole_carapace")
+    table.insert(PrefabFiles, "weevole")
 end
