17f781b523d6a74c32d59513996a8f2998b536a4
LunarSolar <kageno13927@yeah.net>
2025-05-18 16:14:10 +0800

    实现城镇之钥实时地图显示

--- scripts/prefabs/key_to_city.lua
+++ scripts/prefabs/key_to_city.lua
@@ -19,18 +19,61 @@ local function canCurrentlyPrototypeTestFn(inst)
     return not TheCamera.interior
 end
 
+local function storeincontainer(inst, container)
+    if container ~= nil and container.components.container ~= nil then
+        inst:ListenForEvent("onputininventory", inst._oncontainerownerchanged, container)
+        inst:ListenForEvent("ondropped", inst._oncontainerownerchanged, container)
+        inst:ListenForEvent("onremove", inst._oncontainerremoved, container)
+        inst._container = container
+    end
+end
+
+local function unstore(inst)
+    if inst._container ~= nil then
+        inst:RemoveEventCallback("onputininventory", inst._oncontainerownerchanged, inst._container)
+        inst:RemoveEventCallback("ondropped", inst._oncontainerownerchanged, inst._container)
+        inst:RemoveEventCallback("onremove", inst._oncontainerremoved, inst._container)
+        inst._container = nil
+    end
+end
+
+local function tostore(inst, owner)
+    if inst._container ~= owner then
+        unstore(inst)
+        storeincontainer(inst, owner)
+    end
+    owner = owner.components.inventoryitem ~= nil and owner.components.inventoryitem:GetGrandOwner() or owner
+    if inst._owner ~= owner then
+        inst._owner = owner
+        inst.icon.entity:SetParent(owner.entity)
+    end
+end
+
+local function topocket(inst, owner)
+    tostore(inst, owner)
+end
+
+local function toground(inst)
+    unstore(inst)
+    inst._owner = nil
+    inst.icon.entity:SetParent(inst.entity)
+end
+
+local function OnRemoveEntity(inst)
+    if inst.icon ~= nil then
+        inst.icon:Remove()
+    end
+end
+
 local function fn(Sim)
     local inst = CreateEntity()
     inst.entity:AddTransform()
     inst.entity:AddAnimState()
     MakeInventoryPhysics(inst)
-    inst.entity:AddMiniMapEntity()
     inst.entity:AddNetwork()
     inst.entity:AddSoundEmitter()
 
     MakeInventoryFloatable(inst)
-
-    inst.MiniMapEntity:SetIcon("key_to_city.png")
 
     inst.AnimState:SetBank("keytocity")
     inst.AnimState:SetBuild("key_to_city")
@@ -45,11 +88,26 @@ local function fn(Sim)
     if not TheWorld.ismastersim then
         return inst
     end
+    
+    inst._owner = nil
+    inst._container = nil
+
+    inst._oncontainerownerchanged = function(container)
+        tostore(inst, container)
+    end
+
+    inst._oncontainerremoved = function()
+        unstore(inst)
+    end
 
     inst:AddComponent("inspectable")
 
     inst:AddComponent("inventoryitem")
 
+    inst.icon = SpawnPrefab("key_to_city_icon")
+    inst.icon.entity:SetParent(inst.entity)
+    inst:ListenForEvent("onputininventory", topocket)
+    inst:ListenForEvent("ondropped", toground)
 
     inst:AddComponent("prototyper")
     inst.components.prototyper.onturnon = OnTurnOn
@@ -59,4 +117,39 @@ local function fn(Sim)
     return inst
 end
 
-return Prefab("key_to_city", fn, assets)
+local function icon_init(inst)
+    inst.icon = SpawnPrefab("globalmapicon")
+    inst.icon.MiniMapEntity:SetPriority(11)
+    inst.icon:TrackEntity(inst)
+end
+
+local function iconfn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddMiniMapEntity()
+    inst.entity:AddNetwork()
+
+    inst.MiniMapEntity:SetIcon("key_to_city.tex")
+    inst.MiniMapEntity:SetPriority(11)
+    inst.MiniMapEntity:SetCanUseCache(false)
+    inst.MiniMapEntity:SetDrawOverFogOfWar(true)
+
+    inst:AddTag("CLASSIFIED")
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst.icon = nil
+    inst:DoTaskInTime(0, icon_init)
+    inst.OnRemoveEntity = inst.OnRemoveEntity
+    inst.persists = false
+
+    return inst
+end
+
+return Prefab("key_to_city", fn, assets),
+    Prefab("key_to_city_icon", iconfn)
