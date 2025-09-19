33497abc7848ddb58ce3c3dfadb9478abab300a0
momo <niuzw18@foxmail.com>
2025-09-19 21:30:20 +0800

    更新版本号

--- modinfo.lua
+++ modinfo.lua
@@ -15,13 +15,13 @@ local isdev = not folder_name:find("workshop-")
 name = not isdev and en_zh_zht("Tropical Experience | SW HAM Biomes : From Beyond", "热带体验 | 海难哈姆雷特生态：来自域外", "船難哈姆雷特生態：來自域外")
         or "Tropical Experience | Classic Dev"
 description = en_zh_zht(
-	"\nversion：3.96" ..
+	"\nversion：3.97" ..
 	"\nAttention: We added a complement to this mod." ..
 	"\nIn it will have several changes to improve the experience of the game." ..
 	"\nVisit only the main mod page and download." ..
 	"\nTropical Experience| Complement",
 
-	"\n版本：3.96" ..
+	"\n版本：3.97" ..
 	"\n注意：我们为此模组添加了一些内容补充" ..
 	"\n其中包含了多项改进游戏体验的玩法变化，请访问此模组创意工坊进行下载" ..
 	"\n多种生态群系体验 | 补充内容" ..
@@ -33,7 +33,7 @@ description = en_zh_zht(
 	"\n大风平原(Windy Plains)" ..
 	"\n\n兼容樱花林(Cherry Forest)",
 
-	"\n版本：3.96" ..
+	"\n版本：3.97" ..
 	"\n注意：我們爲此模組添加了一些內容補充" ..
 	"\n其中包含了多項改進遊戲體驗的玩法變化，請訪問此模組創意工坊進行下載" ..
 	"\n多種生態羣系體驗 | 補充內容" ..
@@ -47,7 +47,7 @@ description = en_zh_zht(
 )
 
 author = "Vagner da Rocha Santos."
-version = "3.96"
+version = "3.97"
 forumthread = ""
 api_version = 10
 priority = -20
