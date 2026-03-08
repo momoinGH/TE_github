06ece7c6eab49079ebfab05b45f7c2c925fff8f0
gufengchangdao <1580313824@qq.com>
2026-03-09 01:11:15 +0800

    修复中毒动画

--- modmain/common/assets.lua
+++ modmain/common/assets.lua
@@ -3,20 +3,20 @@ Assets =
     Asset("ANIM", "anim/pigman_tribe.zip"), --图鉴wiki默认动画
 
     --LOAD SOUND FILE
-	Asset("SOUNDPACKAGE", "sound/dontstarve_DLC002.fev"),
-	Asset("SOUND", "sound/music_stream_SW.fsb"),
-	Asset("SOUND", "sound/dontstarve_shipwreckedSFX.fsb"),
+    Asset("SOUNDPACKAGE", "sound/dontstarve_DLC002.fev"),
+    Asset("SOUND", "sound/music_stream_SW.fsb"),
+    Asset("SOUND", "sound/dontstarve_shipwreckedSFX.fsb"),
 
-	Asset("SOUNDPACKAGE", "sound/sw_character.fev"),
-	Asset("SOUND", "sound/sw_character.fsb"),
+    Asset("SOUNDPACKAGE", "sound/sw_character.fev"),
+    Asset("SOUND", "sound/sw_character.fsb"),
 
-	Asset("SOUNDPACKAGE", "sound/dontstarve_DLC003.fev"),
-	Asset("SOUND", "sound/DLC003_sfx.fsb"),
-	Asset("SOUND", "sound/DLC003_music_stream.fsb"),
-	-- Asset("SOUND", "sound/DLC003_AMB_stream.fsb"), --仓库里没有
+    Asset("SOUNDPACKAGE", "sound/dontstarve_DLC003.fev"),
+    Asset("SOUND", "sound/DLC003_sfx.fsb"),
+    Asset("SOUND", "sound/DLC003_music_stream.fsb"),
+    Asset("SOUND", "sound/DLC003_AMB_stream.fsb"),
 
-	Asset("SOUNDPACKAGE", "sound/dontstarve_wagstaff.fev"),
-	Asset("SOUND", "sound/dontstarve_wagstaff.fsb"),
+    Asset("SOUNDPACKAGE", "sound/dontstarve_wagstaff.fev"),
+    Asset("SOUND", "sound/dontstarve_wagstaff.fsb"),
 
     --Asset("SOUNDPACKAGE", "sound/volcano.fev"),
     --Asset("SOUND", "sound/volcano.fsb"),
@@ -33,7 +33,7 @@ Assets =
     --Asset("SOUNDPACKAGE", "sound/Hamlet.fev"),
     --Asset("SOUND", "sound/Hamlet.fsb"),
 
-    Asset("IMAGE", "images/fog_cloud.tex"),--云海
+    Asset("IMAGE", "images/fog_cloud.tex"), --云海
     Asset("ATLAS", "images/barco.xml"),
 
     -------------overlays -------------------
@@ -50,54 +50,54 @@ Assets =
 
     -------------mapstyle -------------------
     Asset("IMAGE", "images/mapstyle/hud_shipwrecked.tex"),
-	Asset("ATLAS", "images/mapstyle/hud_shipwrecked.xml"),
+    Asset("ATLAS", "images/mapstyle/hud_shipwrecked.xml"),
 
     Asset("IMAGE", "images/mapstyle/hud_hamlet.tex"),
-	Asset("ATLAS", "images/mapstyle/hud_hamlet.xml"),
+    Asset("ATLAS", "images/mapstyle/hud_hamlet.xml"),
 
-	Asset("IMAGE", "images/mapstyle/hud_pigcity.tex"),
-	Asset("ATLAS", "images/mapstyle/hud_pigcity.xml"),
+    Asset("IMAGE", "images/mapstyle/hud_pigcity.tex"),
+    Asset("ATLAS", "images/mapstyle/hud_pigcity.xml"),
 
-	-------------colour cubes -------------------
-	Asset("IMAGE", "images/colour_cubes/sw_mild_day_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/SW_mild_dusk_cc.tex"),
+    -------------colour cubes -------------------
+    Asset("IMAGE", "images/colour_cubes/sw_mild_day_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/SW_mild_dusk_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/SW_wet_day_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/SW_wet_dusk_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/SW_wet_day_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/SW_wet_dusk_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/sw_green_day_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/sw_green_dusk_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/sw_green_day_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/sw_green_dusk_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/SW_dry_day_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/SW_dry_dusk_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/SW_dry_day_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/SW_dry_dusk_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/sw_volcano_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/sw_volcano_active_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/sw_volcano_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/sw_volcano_active_cc.tex"),
 
 
-	Asset("IMAGE", "images/colour_cubes/pork_temperate_day_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_temperate_dusk_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_temperate_night_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_temperate_fullmoon_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_temperate_day_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_temperate_dusk_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_temperate_night_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_temperate_fullmoon_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/pork_cold_day_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_cold_dusk_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_cold_fullmoon_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_cold_day_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_cold_dusk_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_cold_fullmoon_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/pork_lush_day_test.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_lush_dusk_test.tex"),
-	Asset("IMAGE", "images/colour_cubes/pork_warm_fullmoon_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_lush_day_test.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_lush_dusk_test.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_warm_fullmoon_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/pork_cold_bloodmoon_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pork_cold_bloodmoon_cc.tex"),
 
-	Asset("IMAGE", "images/colour_cubes/pigshop_interior_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/pigshop_interior_cc.tex"),
     Asset("IMAGE", "images/colour_cubes/bat_vision_on_cc.tex"),
-	Asset("IMAGE", "images/colour_cubes/heat_vision_cc.tex"),
+    Asset("IMAGE", "images/colour_cubes/heat_vision_cc.tex"),
     Asset("IMAGE", "images/colour_cubes/shooting_goggles_cc.tex"),
 
 
     -------------anims -------------------
-    Asset("ANIM", "anim/player_wagstaff.zip" ),
+    Asset("ANIM", "anim/player_wagstaff.zip"),
     Asset("ANIM", "anim/player_mount_wagstaff.zip"),
     Asset("ANIM", "anim/livingartifact_meter.zip"),
     Asset("ANIM", "anim/player_lifeplant.zip"),
@@ -211,23 +211,23 @@ Assets =
 }
 
 -- # No more inventoryitem atlas register
-local InvAtlas = {"frost", "greenworld", "grotto", "hamlet", "lavaarena", "quagmire", "rog", "shipwrecked_plus",
-                         "shipwrecked", "underwater", "windy", "skins"}
+local InvAtlas = { "frost", "greenworld", "grotto", "hamlet", "lavaarena", "quagmire", "rog", "shipwrecked_plus",
+    "shipwrecked", "underwater", "windy", "skins" }
 for i = 1, #InvAtlas do
     InvAtlas[i] = "images/inventoryimages/inventory_" .. InvAtlas[i] .. ".xml"
     table.insert(Assets, Asset("ATLAS", InvAtlas[i]))
     table.insert(Assets, Asset("ATLAS_BUILD", InvAtlas[i], 256)) -- for minisign
     InvAtlas[i] = resolvefilepath(InvAtlas[i])
 end
-local FoodAtlas = {"frost", "ham", "quagmire", "sw", "underwater"}
+local FoodAtlas = { "frost", "ham", "quagmire", "sw", "underwater" }
 for i = 1, #FoodAtlas do
     FoodAtlas[i] = "images/inventoryimages/cookpotfoods/cookpotfoods_" .. FoodAtlas[i] .. ".xml"
     table.insert(Assets, Asset("ATLAS", FoodAtlas[i]))
     table.insert(Assets, Asset("ATLAS_BUILD", FoodAtlas[i], 256)) -- for minisign
     FoodAtlas[i] = resolvefilepath(FoodAtlas[i])
 end
-local MiMapAtlas = {"frost", "volcano", "grotto", "hamlet", "lavaarena", "quagmire", "rog", "shipwrecked_plus",
-                       "shipwrecked", "underwater", "windy"}
+local MiMapAtlas = { "frost", "volcano", "grotto", "hamlet", "lavaarena", "quagmire", "rog", "shipwrecked_plus",
+    "shipwrecked", "underwater", "windy" }
 for i = 1, #MiMapAtlas do
     MiMapAtlas[i] = "minimap/minimap_" .. MiMapAtlas[i] .. ".xml"
     table.insert(Assets, Asset("ATLAS", MiMapAtlas[i]))
