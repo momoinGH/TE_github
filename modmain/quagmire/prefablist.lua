be1bd4153321c49f46561ab432fc1601dae5faa0
gufengchangdao <1580313824@qq.com>
2026-03-15 02:02:25 +0800

    暴食预制件分类整理

--- modmain/quagmire/prefablist.lua
+++ modmain/quagmire/prefablist.lua
@@ -1,62 +1,57 @@
 PrefabFiles =
 {
-    "feather_chicken", --鸡毛
-    "gorge_portal",    --苔藓大门
-    "chicken",         --鸡
-
-
-
-    "quagmire_merm_house",
-    "quagmire_mealingstone",
-    "quagmire_flour",
-    "quagmire_foods",
-    "quagmire_goatmilk",
-    "quagmire_sap",
-    "quagmire_syrup",
-    "quagmire_casseroledish",
-    "quagmire_crates",
-    "quagmire_grill",
-    "quagmire_oven",
-    "quagmire_plates",
-    "quagmire_pot",
-    "quagmire_pot_hanger",
-    "quagmire_salt_rack",
-    "quagmire_sapbucket",
-    "quagmire_slaughtertool",
-    "quagmire_altar",
-    "quagmire_seedpackets",
-    "quagmire_sugarwoodtree",
-    "quagmire_goatmum",
-    "quagmire_swampigelder",
-    "quagmire_oldstructures",
-    "quagmire_lamp_post",
-    "quagmire_altar_statue",
-    "quagmire_portal",
-    "quagmire_crabtrap",
-    "quagmire_smallmeat",
-    "quagmire_mushroomstump",
-    "quagmire_mushrooms",
-    "quagmire_fern",
-    "quagmire_pebblecrab",
-    "quagmire_safe",
-    "quagmire_parkspike",
-    "quagmire_key",
-    "quagmire_goatkid",
-    "quagmire_coins",
-    "quagmire_portal_key",
-    "quagmiregoat",
-    "quagmiregoatherd",
-    "quagmire_swampig",
-    "quagmire_swampig_house",
-    "quagmire_pond_salt",
-    "quagmire_beefalo",
-    "quagmire_sugarwoodtree",
-    "quagmire_sap",
-    "quagmire_sugarwoodtree_sapling",
-    "quagmire_sugarwoodtree_cone",
-    "quagmire_pigeon",
-    "quagmire_spiceshrub",
-    "dug_quagmire_spotspice_shrub",
-    "turf_quagmire",
-    "quagmire_plants_planted",
+    "feather_chicken",                --鸡毛
+    "gorge_portal",                   --苔藓大门
+    "chicken",                        --鸡
+    "quagmire_merm_house",            --破烂的小屋
+    "quagmire_mealingstone",          --碾磨石
+    "quagmire_flour",                 --面粉
+    "quagmire_foods",                 --食物
+    "quagmire_goatmilk",              --羊奶
+    "quagmire_sap",                   --汁液
+    "quagmire_syrup",                 --糖浆
+    "quagmire_casseroledish",         --大法国砂锅
+    "quagmire_crates",                --厨具套装
+    "quagmire_grill",                 --大烤架
+    "quagmire_oven",                  --烤箱
+    "quagmire_plates",                --银盘
+    "quagmire_pot",                   --大锅
+    "quagmire_pot_hanger",            --挂锅装置
+    "quagmire_salt_rack",             --盐架
+    "quagmire_sapbucket",             --树液采集工具包
+    "quagmire_slaughtertool",         --屠宰工具
+    "quagmire_altar",                 --饕餮祭坛
+    "quagmire_seedpackets",           --种子包
+    "quagmire_sugarwoodtree",         --糖木树
+    "quagmire_sugarwoodtree_sapling", --棉树苗
+    "quagmire_sugarwoodtree_cone",    --棉树种子
+    "quagmire_goatmum",               --玛姆茜
+    "quagmire_swampigelder",          --沼泽猪长老
+    "quagmire_oldstructures",         --一堆破烂建筑
+    "quagmire_lamp_post",             --街灯
+    "quagmire_altar_statue",          --饥饿之兽
+    "quagmire_portal",                --远古大门
+    "quagmire_crabtrap",              --螃蟹陷阱
+    "quagmire_smallmeat",             --肉碎
+    "quagmire_mushroomstump",         --蘑菇
+    "quagmire_mushrooms",             --蘑菇
+    "quagmire_fern",                  --蕨类植物
+    "quagmire_pebblecrab",            --石蟹
+    "quagmire_safe",                  --保险箱
+    "quagmire_parkspike",             --铁栅栏
+    "quagmire_key",                   --钥匙
+    "quagmire_goatkid",               --比利
+    "quagmire_coins",                 --钱
+    "quagmire_portal_key",            --远古钥匙
+    "quagmiregoat",                   --沼地山羊
+    "quagmiregoatherd",               --沼地山羊生成器
+    "quagmire_swampig",               --沼泽猪
+    "quagmire_swampig_house",         --沼泽猪舍
+    "quagmire_pond_salt",             --盐池
+    "quagmire_beefalo",               --老皮弗娄牛
+    "quagmire_pigeon",                --鸽子
+    "quagmire_spiceshrub",            --带斑点的小灌木
+    "dug_quagmire_spotspice_shrub",   --带斑点的小灌木丛
+    "turf_quagmire",                  --地皮
+    "quagmire_plants_planted",        --植物
 }
