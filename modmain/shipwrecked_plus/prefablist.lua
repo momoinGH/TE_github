0acaf17865ced615cf8d598dbe6d937cef3f16e7
gufengchangdao <1580313824@qq.com>
2026-03-15 13:42:32 +0800

    海难plus预制件分类整理

--- modmain/shipwrecked_plus/prefablist.lua
+++ modmain/shipwrecked_plus/prefablist.lua
@@ -1,5 +1,5 @@
 PrefabFiles = {
-    "tikistick", --提基头颅
+    "tikistick",        --提基头颅
     "tree_forest_deep", --粉色丛林树
     "tree_forest",      --葱绿丛林树
     "tree_forestseed",  --葱绿丛林树种
@@ -7,18 +7,18 @@ PrefabFiles = {
     "pandaskin",        --熊猫皮
     "pandatree",        --巨竹
 
-    "goldentomb",
-    "goldmonkey",
-    "goldbishop",
-    "goldobi",
-    "tikihead",
-    "teepee",
-    "tikimask",
-    "tikifire",
-    "wanawanatiki",
-    "summerwalrus",
-    "summerigloo",
-    "octoatt",
-    "octopus",
-    "octohouse",
+    "goldentomb",       --黄金球
+    "goldmonkey",       --黄金猴
+    "goldbishop",       --黄金守护者
+    "goldobi",          --黄金方尖碑
+    "tikihead",         --提基雕像
+    "teepee",           --部落帐篷
+    "tikimask",         --提基面具
+    "tikifire",         --提基营火
+    "wanawanatiki",     --哇呐哇呐提基
+    "summerwalrus",     --旅行海象
+    "summerigloo",      --度假营地
+    "octoatt",          --章鱼投射物
+    "octopus",          --章鱼
+    "octohouse",        --章鱼屋
 }
