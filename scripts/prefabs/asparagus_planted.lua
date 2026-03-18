27d152835d881225ecb83e49be92ec2e8dcb3da4
gufengchangdao <1580313824@qq.com>
2026-03-18 20:54:33 +0800

    修复哈姆雷特芦苇采集时报错的问题

--- scripts/prefabs/asparagus_planted.lua
+++ scripts/prefabs/asparagus_planted.lua
@@ -1,54 +1,56 @@
 local assets =
 {
-	Asset("ANIM", "anim/asparagus_planted.zip"),
+    Asset("ANIM", "anim/asparagus_planted.zip"),
 }
 
 local prefabs =
 {
-	"asparagus",
+    "asparagus",
 }
 
 local function onpickedfn(inst)
-	inst:Remove()
+    if inst:IsValid() then
+        inst:DoTaskInTime(0, inst.Remove)
+    end
 end
 
 
 local function fn(Sim)
     --Asparagus you eat is defined in veggies.lua
-	local inst = CreateEntity()
-	inst.entity:AddTransform()
-	inst.entity:AddAnimState()
-	inst.entity:AddSoundEmitter()
+    local inst = CreateEntity()
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
     inst.entity:AddNetwork()
 
-    inst.Transform:SetScale(1.3,1.3,1.3)
+    inst.Transform:SetScale(1.3, 1.3, 1.3)
 
     inst.AnimState:SetBank("asparagus_planted")
     inst.AnimState:SetBuild("asparagus_planted")
     inst.AnimState:PlayAnimation("planted")
     inst.AnimState:SetRayTestOnBB(true)
 
-	MakeInventoryFloatable(inst)
+    MakeInventoryFloatable(inst)
 
     inst.entity:SetPristine()
 
-	if not TheWorld.ismastersim then
-		return inst
-	end
+    if not TheWorld.ismastersim then
+        return inst
+    end
 
     inst:AddComponent("inspectable")
 
     inst:AddComponent("pickable")
     inst.components.pickable.picksound = "dontstarve/wilson/pickup_plants"
     inst.components.pickable:SetUp("asparagus", 10)
-	inst.components.pickable.onpickedfn = onpickedfn
+    inst.components.pickable.onpickedfn = onpickedfn
 
     inst.components.pickable.quickpick = true
 
-	MakeSmallBurnable(inst)
+    MakeSmallBurnable(inst)
     MakeSmallPropagator(inst)
 
     return inst
 end
 
-return Prefab("asparagus_planted", fn, assets)
+return Prefab("asparagus_planted", fn, assets, prefabs)
