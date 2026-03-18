9b2c837db825a27f725b3421a03ffaa1e3d66736
gufengchangdao <1580313824@qq.com>
2026-03-18 21:15:56 +0800

    修复装备工具报错的问题

--- modmain/common/screens/playerhud.lua
+++ modmain/common/screens/playerhud.lua
@@ -5,16 +5,6 @@ AddClassPostConstruct("screens/playerhud", function(self)
     Hooks.FnDecorator(self, "CreateOverlays", nil, function(retTab, self, owner, ...)
         -- 面甲的UI
         self.visorover = self.overlayroot:AddChild(VisorOver(owner))
-        self.visorover:Hide()
-        return retTab
-    end)
-
-    Hooks.FnDecorator(self, "SetMainCharacter", nil, function(retTab, self, maincharacter)
-        if not maincharacter then return retTab end
-
-        self.inst:ListenForEvent("unequip", function(inst, data) self.visorover:UpdateState(data) end, self.owner)
-        self.inst:ListenForEvent("equip", function(inst, data) self.visorover:UpdateState(data) end, self.owner)
-
         return retTab
     end)
 end)
