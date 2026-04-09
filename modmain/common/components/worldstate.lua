078c76b79df227ebad1e29fef707756d9a74e481
gufengchangdao <1580313824@qq.com>
2026-04-09 11:57:11 +0800

    新增一些TheWorld.state.xxx变量

--- modmain/common/components/worldstate.lua
+++ modmain/common/components/worldstate.lua
@@ -1,8 +1,33 @@
 -- 定义一些TheWorld.state.xxx变量，用的时候可能还得判断模块的地形，可以先看看EntityScript有没有封装给实体判断用的方法
 
 AddComponentPostInit("worldstate", function(self, inst)
-    local OnTemperatureTick = Hooks.GetEventCallback(inst, "temperaturetick", inst, "scripts/components/worldstate.lua")
-    local SetVariable = Hooks.GetUpvalue(OnTemperatureTick, "SetVariable")
+    local _watchers = Hooks.GetUpValue(self.AddWatcher, "_watchers")
+
+    local function SetVariable(var, val, togglename)
+        if self.data[var] ~= val and val ~= nil then
+            self.data[var] = val
+
+            local watchers = _watchers[var]
+            if watchers ~= nil then
+                for k, v in pairs(watchers) do
+                    for i, fn in ipairs(v) do
+                        fn[1](fn[2], val)
+                    end
+                end
+            end
+
+            if togglename then
+                watchers = _watchers[(val and "start" or "stop") .. togglename]
+                if watchers ~= nil then
+                    for k, v in pairs(watchers) do
+                        for i, fn in ipairs(v) do
+                            fn[1](fn[2])
+                        end
+                    end
+                end
+            end
+        end
+    end
 
     local function OnSeasonChange(inst, season)
         print("季节变化", season)
