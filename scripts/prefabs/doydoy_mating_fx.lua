4e072f3bad6d1e43a0f2795f62cc704b66262c42
gufengchangdao <1580313824@qq.com>
2026-04-11 19:34:29 +0800

    渡渡鸟代码迭代

--- scripts/prefabs/doydoy_mating_fx.lua
+++ scripts/prefabs/doydoy_mating_fx.lua
@@ -1,45 +1,43 @@
 local assets =
 {
-	Asset("ANIM", "anim/doydoy_mate_fx.zip"),
+    Asset("ANIM", "anim/doydoy_mate_fx.zip"),
 }
 
-local function onremove(inst)
-	inst.SoundEmitter:KillSound("voice")
-	inst.SoundEmitter:KillSound("mating")
-end
+local function fn()
+    local inst = CreateEntity()
 
-local function fn(Sim)
-	local inst = CreateEntity()
-	inst.entity:AddTransform()
-	inst.entity:AddAnimState()
-	inst.entity:AddSoundEmitter()
-	inst.entity:AddNetwork()
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
+    inst.entity:AddNetwork()
 
-	inst.Transform:SetScale(1.2, 1.2, 1.2)
+    inst.Transform:SetScale(1.2, 1.2, 1.2)
 
-	inst.AnimState:SetBank("doydoy_mate_fx")
-	inst.AnimState:SetBuild("doydoy_mate_fx")
-	inst.AnimState:SetSortOrder(-1)
-	inst:AddTag("NOCLICK")
+    inst.AnimState:SetBank("doydoy_mate_fx")
+    inst.AnimState:SetBuild("doydoy_mate_fx")
+    inst.AnimState:SetSortOrder(-1)
+    inst.AnimState:PlayAnimation("mate_pre")
+    inst.AnimState:PushAnimation("mate_loop")
+    inst.AnimState:PushAnimation("mate_loop")
+    inst.AnimState:PushAnimation("mate_loop")
+    inst.AnimState:PushAnimation("mate_pst", false)
 
-	inst.AnimState:PlayAnimation("mate_pre")
-	inst.AnimState:PushAnimation("mate_loop")
-	inst.AnimState:PushAnimation("mate_loop")
-	inst.AnimState:PushAnimation("mate_loop")
-	inst.AnimState:PushAnimation("mate_pst", false)
+    inst.SoundEmitter:PlaySound("dontstarve_DLC002/doy_doy/mating_dance_LP", "voice")
+    inst.SoundEmitter:PlaySound("dontstarve_DLC002/doy_doy/mate_dance_post", "mating")
 
-	if not TheWorld.ismastersim then
-		return inst
-	end
+    inst:AddTag("NOCLICK")
+    inst:AddTag("FX")
 
-	inst:ListenForEvent("animqueueover", function(inst) inst:Remove() end)
+    inst.entity:SetPristine()
 
-	inst.SoundEmitter:PlaySound("dontstarve_DLC002/doy_doy/mating_dance_LP", "voice")
-	inst.SoundEmitter:PlaySound("dontstarve_DLC002/doy_doy/mate_dance_post", "mating")
+    if not TheWorld.ismastersim then
+        return inst
+    end
 
-	inst.entity:SetPristine()
+    inst:ListenForEvent("animqueueover", inst.Remove)
+    inst.persists = false
 
-	return inst
+    return inst
 end
 
 return Prefab("doydoy_mate_fx", fn, assets)
