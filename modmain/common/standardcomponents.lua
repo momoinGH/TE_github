a7219f6acfcfe5228d5546e8b6980d799048de61
无往回首 <1580313824@qq.com>
2026-07-14 22:37:10 +0800

    修复出生点配置一些bug

--- modmain/common/standardcomponents.lua
+++ modmain/common/standardcomponents.lua
@@ -147,3 +147,132 @@ GLOBAL.MakeSnowCovered = function(inst, ...)
         end
     end)
 end
+
+----------------------------------------------------------------------------------------------------
+-- 海难飓风
+local function ongustpick(inst)
+    if inst.components.pickable and inst.components.pickable:CanBePicked() then
+        inst.components.pickable:Pick(inst)
+    end
+end
+
+-- 针对可采集的植物，添加刮风处理，播放刮风动画
+function _G.MakePickableBlowInWindGust(inst, wind_speed, destroy_chance, done_anim)
+    inst.tro_hurricane_done_anim = done_anim or "idle"
+
+    local function onblownpstdone(inst)
+        if inst.components.pickable and
+            inst.components.pickable:CanBePicked() and
+            (
+                inst.AnimState:IsCurrentAnimation("blown_pst") or
+                inst.AnimState:IsCurrentAnimation("blown_loop") or
+                inst.AnimState:IsCurrentAnimation("blown_pre")
+            )
+        then
+            inst.AnimState:PlayAnimation(inst.tro_hurricane_done_anim, true)
+        end
+        inst:RemoveEventCallback("animover", onblownpstdone)
+    end
+
+    local function ongustanimdone(inst)
+        if inst.components.pickable and inst.components.pickable:CanBePicked() then
+            if inst.components.blowinwindgust:IsGusting() then
+                local anim = math.random(1, 2)
+                inst.AnimState:PlayAnimation("blown_loop" .. anim, false)
+            else
+                inst:DoTaskInTime(math.random() / 2, function(inst)
+                    inst:RemoveEventCallback("animover", ongustanimdone)
+
+                    -- This may not be true anymore
+                    if inst.components.pickable and inst.components.pickable:CanBePicked() then
+                        inst.AnimState:PlayAnimation("blown_pst", false)
+                        -- changed this from a push animation to an animover listen event so that it can be interrupted if necessary, and that a check can be made at the end to know if it should go to idle at that time.
+                        --inst.AnimState:PushAnimation("idle", true)
+                        inst:ListenForEvent("animover", onblownpstdone)
+                    end
+                end)
+            end
+        else
+            inst:RemoveEventCallback("animover", ongustanimdone)
+        end
+    end
+    local function onguststart(inst)
+        inst:DoTaskInTime(math.random() / 2, function(inst)
+            if inst.components.pickable and inst.components.pickable:CanBePicked() then
+                inst.AnimState:PlayAnimation("blown_pre", false)
+                inst:ListenForEvent("animover", ongustanimdone)
+            end
+        end)
+    end
+
+    inst:AddComponent("blowinwindgust")
+    inst.components.blowinwindgust:SetWindSpeedThreshold(wind_speed)
+    inst.components.blowinwindgust:SetDestroyChance(destroy_chance)
+    inst.components.blowinwindgust:SetGustStartFn(onguststart)
+    inst.components.blowinwindgust:SetGustEndFn(onblownpstdone)
+    inst.components.blowinwindgust:SetDestroyFn(ongustpick)
+    inst.components.blowinwindgust:Start()
+end
+
+function _G.MakeHackableBlowInWindGust(inst, wind_speed, destroy_chance)
+    local function onblownpstdone(inst)
+        if inst.components.hackable and
+            inst.components.hackable:CanBeHacked() and
+            (
+                inst.AnimState:IsCurrentAnimation("blown_pst") or
+                inst.AnimState:IsCurrentAnimation("blown_loop") or
+                inst.AnimState:IsCurrentAnimation("blown_pre")
+            )
+        then
+            inst.AnimState:PlayAnimation("idle", true)
+        end
+        inst:RemoveEventCallback("animover", onblownpstdone)
+    end
+
+    local function ongustanimdone(inst)
+        if inst.components.hackable and inst.components.hackable:CanBeHacked() then
+            if inst.components.blowinwindgust:IsGusting() then
+                local anim = math.random(1, 2)
+                inst.AnimState:PlayAnimation("blown_loop" .. anim, false)
+            else
+                inst:DoTaskInTime(math.random() / 2, function(inst)
+                    inst:RemoveEventCallback("animover", ongustanimdone)
+
+                    -- This may not be true anymore
+                    if inst.components.hackable and inst.components.hackable:CanBeHacked() then
+                        inst.AnimState:PlayAnimation("blown_pst", false)
+                        -- changed this from a push animation to an animover listen event so that it can be interrupted if necessary, and that a check can be made at the end to know if it should go to idle at that time.
+                        --inst.AnimState:PushAnimation("idle", true)
+                        inst:ListenForEvent("animover", onblownpstdone)
+                    end
+                end)
+            end
+        else
+            inst:RemoveEventCallback("animover", ongustanimdone)
+        end
+    end
+
+    local function onguststart(inst, windspeed)
+        inst:DoTaskInTime(math.random() / 2, function(inst)
+            if inst.components.hackable and inst.components.hackable:CanBeHacked() then
+                inst.AnimState:PlayAnimation("blown_pre", false)
+                inst:ListenForEvent("animover", ongustanimdone)
+            end
+        end)
+    end
+
+    local function ongusthack(inst)
+        if inst.components.lootdropper and inst.components.hackable and inst.components.hackable:CanBeHacked() then
+            inst.components.hackable:MakeEmpty()
+            inst.components.lootdropper:SpawnLootPrefab(inst.components.hackable.product)
+        end
+    end
+
+    inst:AddComponent("blowinwindgust")
+    inst.components.blowinwindgust:SetWindSpeedThreshold(wind_speed)
+    inst.components.blowinwindgust:SetDestroyChance(destroy_chance)
+    inst.components.blowinwindgust:SetGustStartFn(onguststart)
+    inst.components.blowinwindgust:SetGustEndFn(onblownpstdone)
+    inst.components.blowinwindgust:SetDestroyFn(ongusthack)
+    inst.components.blowinwindgust:Start()
+end
